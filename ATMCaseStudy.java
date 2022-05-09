e63210f607bb1b95aabd5bbb1abea69ff99df16d
ayrin21 <65002390+ayrin21@users.noreply.github.com>
2022-05-09 16:58:23 +0600

    Add files via upload

---
+++ ATMCaseStudy.java
@@ -0,0 +1,14 @@
+
+public class ATMCaseStudy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+   ATM theATM = new ATM();
+   theATM.run();
+  // end main
+    }
+    
+}
